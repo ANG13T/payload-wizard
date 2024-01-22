6db4a7d7c873107051b8269161c715a000750dea
angelina-tsuboi <angelina.t1832@gmail.com>
2024-01-22 14:35:34 -0800

    feat: init logo and color

--- components/TextBlock.tsx
+++ components/TextBlock.tsx
@@ -2,18 +2,21 @@ interface Props {
   text: string;
   editable?: boolean;
   onChange?: (value: string) => void;
+  defaultText?: string;
 }
 
 export const TextBlock: React.FC<Props> = ({
   text,
   editable = false,
   onChange = () => {},
+  defaultText = ""
 }) => {
   return (
     <textarea
       className="min-h-[500px] w-full bg-[#1A1B26] p-4 text-[15px] text-neutral-200 focus:outline-none"
       style={{ resize: 'none' }}
       value={text}
+      placeholder={defaultText}
       onChange={(e) => onChange(e.target.value)}
       disabled={!editable}
     />
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,15 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [inputLanguage, setInputLanguage] = useState<string>('JavaScript');
+  // Generation Code
+  const [inputGenerationPrompt, setInputGenerationPrompt] = useState<string>('');
   const [outputLanguage, setOutputLanguage] = useState<string>('Python');
-  const [inputCode, setInputCode] = useState<string>('');
   const [outputCode, setOutputCode] = useState<string>('');
+
+  // Interepreter Code
+  const [inputCode, setInputCode] = useState<string>('');
+  const [outputFeedback, setOutputFeedback] = useState<string>('');
+
   const [model, setModel] = useState<OpenAIModel>('gpt-3.5-turbo');
   const [loading, setLoading] = useState<boolean>(false);
   const [hasTranslated, setHasTranslated] = useState<boolean>(false);
@@ -50,13 +55,13 @@ export default function Home() {
       return;
     }
 
-    if (inputLanguage === outputLanguage) {
-      alert('Please select different languages.');
+    if (!inputCode) {
+      alert('Please enter payload code.');
       return;
     }
 
-    if (!inputCode) {
-      alert('Please enter some code.');
+    if (!inputGenerationPrompt) {
+      alert('Please enter payload specifications.');
       return;
     }
 
@@ -67,13 +72,20 @@ export default function Home() {
       return;
     }
 
+    if (inputGenerationPrompt.length > maxCodeLength) {
+      alert(
+        `Please enter prompt less than ${maxCodeLength} characters. You are currently at ${inputCode.length} characters.`,
+      );
+      return;
+    }
+
     setLoading(true);
     setOutputCode('');
 
     const controller = new AbortController();
 
     const body: TranslateBody = {
-      inputLanguage,
+      inputLanguage: "",
       outputLanguage,
       inputCode,
       model,
@@ -161,154 +173,166 @@ export default function Home() {
           content="Use AI to translate code from one language to another."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪄</text></svg>"></link>
       </Head>
       <div className="flex h-full min-h-screen flex-col items-center bg-[#0E1117] px-4 pb-20 text-neutral-200 sm:px-10">
 
-  <Link href="https://github.com/ANG13T/payload-wizard" target="_blank">
-  <button type="button" class="absolute top-5 right-5 text-white border-white-700 border-2 bg-black-700 hover:bg-[#8933b6] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-  onClick={() => backToHome()}>
-<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-<path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
-</svg>
-</button>
-</Link>
+        <Link href="https://github.com/ANG13T/payload-wizard" target="_blank">
+          <button type="button" className="absolute top-5 right-5 text-white border-white-700 border-2 bg-black-700 hover:bg-[#8933b6] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onClick={() => backToHome()}>
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd" />
+            </svg>
+          </button>
+        </Link>
 
- { !isHome && 
-  <Link href="/">
-  <button type="button" onClick={() => backToHome()} class="absolute top-5 left-5 text-white border-white-700 border-2 bg-black-700 hover:bg-[#8933b6] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-</svg>
-</button>
-</Link>
-}
-            <div className="mt-10 flex flex-col items-center justify-center sm:mt-20">
+        {!isHome &&
+          <Link href="/">
+            <button type="button" onClick={() => backToHome()} className="absolute top-5 left-5 text-white border-white-700 border-2 bg-black-700 hover:bg-[#8933b6] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+              </svg>
+            </button>
+          </Link>
+        }
+        <div className="mt-10 flex flex-col items-center justify-center sm:mt-20">
           <img className="h-[300px]" src="/logo.png" />
           <img className="h-[50px] mt-10" src="/text.png" />
         </div>
-      
-      { isHome && 
-      <div className="w-full flex flex-col items-center mt-5">
-          <button
-            className="w-[600px] mt-10 text-xl font-mono cursor-pointer rounded-md bg-[#8933b6] px-4 border border-white-700 px-4 py-4 font-bold hover:bg-violet-600 active:bg-violet-700"
-            onClick={() => togglePayloadGeneration()}
-          >
-          Payload Generation   🪄
-          </button>
 
-          <button
-            className="w-[600px] mt-10 text-xl font-mono cursor-pointer rounded-md  bg-[#8933b6] px-4 border border-white-700 px-4 py-4 font-bold hover:bg-violet-600 active:bg-violet-700"
-            onClick={() => togglePayloadInterpreter()}
-          >
-          Payload Interpreter   ✨
-          </button>
+        {isHome &&
+          <div className="w-full flex flex-col items-center mt-5">
+            <button
+              className="w-[600px] mt-10 text-xl font-mono cursor-pointer rounded-md bg-[#8933b6] px-4 border border-white-700 px-4 py-4 font-bold hover:bg-violet-600 active:bg-violet-700"
+              onClick={() => togglePayloadGeneration()}
+            >
+              Payload Generation   🪄
+            </button>
+
+            <button
+              className="w-[600px] mt-10 text-xl font-mono cursor-pointer rounded-md  bg-[#8933b6] px-4 border border-white-700 px-4 py-4 font-bold hover:bg-violet-600 active:bg-violet-700"
+              onClick={() => togglePayloadInterpreter()}
+            >
+              Payload Interpreter   ✨
+            </button>
 
 
-      </div>}
-       { !isHome && 
-        <div className="w-full flex flex-col items-center">
-        <div className="mt-6 text-center text-sm">
-          <APIKeyInput apiKey={apiKey} onChange={handleApiKeyChange} />
-        </div>
+          </div>}
+        {!isHome &&
+          <div className="w-full flex flex-col items-center">
+            <div className="w-[1000px] flex flex-col items-center">
+              <div className="mt-6 mb-3 text-center text-sm">
+                <APIKeyInput apiKey={apiKey} onChange={handleApiKeyChange} />
+              </div>
 
-        <div className="mt-2 flex items-center space-x-2">
-          <ModelSelect model={model} onChange={(value) => setModel(value)} />
+              <div className="mt-2 flex items-center space-x-2">
+                <ModelSelect model={model} onChange={(value) => setModel(value)} />
 
-          <button
-            className="w-[140px] cursor-pointer rounded-md bg-violet-500 px-4 py-2 font-bold hover:bg-violet-600 active:bg-violet-700"
-            onClick={() => handleTranslate()}
-            disabled={loading}
-          >
-            {loading ? 'Translating...' : 'Translate'}
-          </button>
-        </div>
+                <button
+                  className="w-[200px] cursor-pointer rounded-md bg-violet-500 px-4 py-2 font-bold hover:bg-violet-600 active:bg-violet-700"
+                  onClick={() => handleTranslate()}
+                  disabled={loading}
+                >
+                  {(loading && !isGeneration) && 'Interpreting'}
+                  {(loading && isGeneration) && 'Generating'}
+                  {(!loading && !isGeneration) && 'Interpret Payload'}
+                  {(!loading && isGeneration) && 'Generate Payload'}
 
-        <div className="mt-2 text-center text-xs">
-          {loading
-            ? 'Translating...'
-            : hasTranslated
-            ? 'Output copied to clipboard!'
-            : 'Enter some code and click "Translate"'}
-        </div>
+                </button>
+              </div>
+            </div>
 
-      { isGeneration &&
-        <div className="mt-6 flex w-full max-w-[1200px] flex-col justify-between sm:flex-row sm:space-x-4">
-          <div className="h-100 flex flex-col justify-center space-y-2 sm:w-2/4">
-            <div className="text-center text-xl font-bold">Input</div>
+            <div className="mt-3 text-center text-xs">
 
-            <TextBlock
-                text={inputCode}
-                editable={!loading}
-                onChange={(value) => {
-                  setInputCode(value);
-                  setHasTranslated(false);
-                }}
-              />
+
+              {(loading && !isGeneration) && 'Interpreting...'}
+              {(loading && isGeneration) && 'Generating...'}
+              {(hasTranslated) && 'Output copied to clipboard!'}
+              {(!hasTranslated && isGeneration) && 'Enter input and click "Generate Payload"'}
+              {(!hasTranslated && !isGeneration) && 'Enter a payload and click "Interpret Payload"'}
+
+            </div>
+
+            {isGeneration &&
+              <div className="mt-6 flex w-full max-w-[1200px] flex-col justify-between sm:flex-row sm:space-x-4">
+                <div className="h-100 flex flex-col justify-center space-y-2 sm:w-2/4">
+                  <div className="text-center text-xl font-bold mb-2">Input</div>
+
+                  <TextBlock
+                    text={inputCode}
+                    editable={!loading}
+                    onChange={(value) => {
+                      setInputCode(value);
+                      setHasTranslated(false);
+                    }}
+                    defaultText="Input payload functionality here..."
+                  />
+
+                  <div className="h-12"></div>
+
+                </div>
+                <div className="mt-8 flex h-full flex-col justify-center space-y-2 sm:mt-0 sm:w-2/4">
+                  <div className="text-center text-xl font-bold">Output</div>
+
+                  <LanguageSelect
+                    language={outputLanguage}
+                    onChange={(value) => {
+                      setOutputLanguage(value);
+                      setOutputCode('');
+                    }}
+                  />
+
+                  {outputLanguage === 'Natural Language' ? (
+                    <TextBlock text={outputCode} />
+                  ) : (
+                    <CodeBlock code={outputCode} />
+                  )}
+                </div>
+              </div>
+            }
+
+
+            {!isGeneration &&
+              <div className="mt-6 flex w-full max-w-[1200px] flex-col justify-between sm:flex-row sm:space-x-4">
+                <div className="h-100 flex flex-col justify-center space-y-2 sm:w-2/4">
+                  <div className="text-center text-xl font-bold">Payload</div>
+
+                  <CodeBlock
+                    code={inputCode}
+                    editable={!loading}
+                    onChange={(value) => {
+                      setInputCode(value);
+                      setHasTranslated(false);
+                    }}
+                    defaultText="Input payload code here..."
+                  />
+
+                </div>
+                <div className="mt-8 flex h-full flex-col justify-center space-y-2 sm:mt-0 sm:w-2/4">
+                  <div className="text-center text-xl font-bold">Output</div>
+
+                  {outputLanguage === 'Natural Language' ? (
+                    <TextBlock text={outputCode} />
+                  ) : (
+                    <CodeBlock code={outputCode} />
+                  )}
+                </div>
+              </div>
+            }
+
 
           </div>
-          <div className="mt-8 flex h-full flex-col justify-center space-y-2 sm:mt-0 sm:w-2/4">
-            <div className="text-center text-xl font-bold">Output</div>
-
-            <LanguageSelect
-              language={outputLanguage}
-              onChange={(value) => {
-                setOutputLanguage(value);
-                setOutputCode('');
-              }}
-            />
-
-            {outputLanguage === 'Natural Language' ? (
-              <TextBlock text={outputCode} />
-            ) : (
-              <CodeBlock code={outputCode} />
-            )}
-          </div>
-        </div>
-        }
-
-
-        { !isGeneration &&
-        <div className="mt-6 flex w-full max-w-[1200px] flex-col justify-between sm:flex-row sm:space-x-4">
-          <div className="h-100 flex flex-col justify-center space-y-2 sm:w-2/4">
-            <div className="text-center text-xl font-bold">Payload</div>
-
-              <CodeBlock
-                code={inputCode}
-                editable={!loading}
-                onChange={(value) => {
-                  setInputCode(value);
-                  setHasTranslated(false);
-                }}
-                defaultText="Input payload code here..."
-              />
-
-          </div>
-          <div className="mt-8 flex h-full flex-col justify-center space-y-2 sm:mt-0 sm:w-2/4">
-            <div className="text-center text-xl font-bold">Output</div>
-
-            {outputLanguage === 'Natural Language' ? (
-              <TextBlock text={outputCode} />
-            ) : (
-              <CodeBlock code={outputCode} />
-            )}
-          </div>
-        </div>
-        }
-
-
-        </div>
         }
 
 
 
       </div>
 
-      <footer class="bg-[#0e1117]">
-    <div class="w-full max-w-screen-xl mx-auto p-4">
-        <span class="block text-sm text-white sm:text-center font-mono">Developed by <a href="https://angelinatsuboi.com" target="_blank" class="hover:underline">Angelina Tsuboi</a> for <a href="https://stellaryxlabs.com" target="_blank" class="hover:underline">Stellaryx Labs</a></span>
-    </div>
-</footer>
+      <footer className="bg-[#0e1117]">
+        <div className="w-full max-w-screen-xl mx-auto p-4">
+          <span className="block text-sm text-white sm:text-center font-mono">Developed by <a href="https://angelinatsuboi.com" target="_blank" className="underline">Angelina Tsuboi</a> for <a href="https://stellaryxlabs.com" target="_blank" className="underline">Stellaryx Labs</a></span>
+        </div>
+      </footer>
 
     </>
   );
